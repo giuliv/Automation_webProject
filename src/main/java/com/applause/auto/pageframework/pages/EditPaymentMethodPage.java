ea89ea9422e237531c636ecb9ceb36d297d6a179
Andrew Davis <adavis@applausemail.com>
2018-08-28 02:55:27 -0700

    Edit Payment Method Test

---
+++ src/main/java/com/applause/auto/pageframework/pages/EditPaymentMethodPage.java
@@ -0,0 +1,69 @@
+package com.applause.auto.pageframework.pages;
+
+import com.applause.auto.framework.pageframework.util.logger.LogController;
+import com.applause.auto.framework.pageframework.web.AbstractPage;
+import com.applause.auto.framework.pageframework.web.PageFactory;
+import com.applause.auto.framework.pageframework.web.WebElementLocator;
+import com.applause.auto.framework.pageframework.web.factory.WebDesktopImplementation;
+import com.applause.auto.framework.pageframework.web.factory.WebPhoneImplementation;
+import com.applause.auto.framework.pageframework.web.factory.WebTabletImplementation;
+import com.applause.auto.framework.pageframework.webcontrols.Button;
+import com.applause.auto.framework.pageframework.webcontrols.EditField;
+import com.applause.auto.framework.pageframework.webcontrols.Text;
+
+import java.lang.invoke.MethodHandles;
+import java.util.Date;
+
+@WebDesktopImplementation(EditPaymentMethodPage.class)
+@WebTabletImplementation(EditPaymentMethodPage.class)
+@WebPhoneImplementation(EditPaymentMethodPage.class)
+public class EditPaymentMethodPage extends AbstractPage {
+    protected final static LogController LOGGER = new LogController(MethodHandles.lookup().getClass());
+
+    @Override
+    protected void waitUntilVisible() {
+        syncHelper.waitForElementToAppear(getViewSignature());
+    }
+
+    // Public actions
+
+    /**
+     * Enter Name on Card
+     *
+     * @param name
+     * @return String
+     */
+    public String enterNameOnCard(String name) {
+        LOGGER.info("Entering Name on Card");
+        name = getTimestamp(name);
+        getNameOnCardField().setText(name);
+        return name;
+    }
+
+    /**
+     * Click Save Payment Method Button
+     *
+     * @return PaymentMethodsPage
+     */
+    public PaymentMethodsPage clickSavePaymentMethod() {
+        LOGGER.info("Clicking Save Payment Method");
+        getSavePaymentMethodButton().click();
+        return PageFactory.create(PaymentMethodsPage.class);
+    }
+
+    // Protected getters
+    @WebElementLocator(webDesktop = "//h1[text()='Edit a Payment Method']")
+    protected Text getViewSignature() { return new Text(this, getLocator(this, "getViewSignature")); }
+
+    @WebElementLocator(webDesktop = "#NameOnCard")
+    protected EditField getNameOnCardField() { return new EditField(this, getLocator(this, "getNameOnCardField")); }
+
+    @WebElementLocator(webDesktop = "#send2")
+    protected Button getSavePaymentMethodButton() { return new Button(this, getLocator(this, "getSavePaymentMethodButton")); }
+
+    private String getTimestamp(String name) {
+        Date date = new Date();
+        String time = Long.toString(date.getTime());
+        return name + time;
+    }
+}
