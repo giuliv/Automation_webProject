3ecf285709dcbad3bb5e0f521f56d38fbbd14783
Andrew Davis <adavis@applausemail.com>
2018-09-12 23:01:50 -0700

    safari

--- src/main/java/com/applause/auto/pageframework/pages/PaypalLoginPage.java
+++ src/main/java/com/applause/auto/pageframework/pages/PaypalLoginPage.java
@@ -70,7 +70,7 @@ public class PaypalLoginPage extends AbstractPage {
     @WebElementLocator(webDesktop = "//*[@id=\"content\"]")
     protected Image getViewSignature() { return new Image(this, getLocator(this, "getViewSignature")); }
 
-    @WebElementLocator(webDesktop = "input#email")
+    @WebElementLocator(webDesktop = "#login_emaildiv > div.fieldWrapper > input")
     protected EditField getEmailField() { return new EditField(this, getLocator(this, "getEmailField")); }
 
     @WebElementLocator(webDesktop = "//*[@id=\"btnNext\"]")
