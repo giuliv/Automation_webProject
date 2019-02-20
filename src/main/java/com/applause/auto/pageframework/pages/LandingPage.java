9ecadb1a29c02eda48ca51384762da704f67b4de
Eugen Degtyarev <ujnatg@gmail.com>
2019-02-20 04:19:55 +0200

    PEET-46

--- src/main/java/com/applause/auto/pageframework/pages/LandingPage.java
+++ src/main/java/com/applause/auto/pageframework/pages/LandingPage.java
@@ -152,7 +152,7 @@ public class LandingPage extends AbstractPage {
 	private void dismissPopup() {
 		try {
 			LOGGER.info("Attempting to dismiss popup");
-			syncHelper.waitForElementToAppear(getLocator(this, "getDismissPopupButton"), 120, 10, getDriver());
+			syncHelper.waitForElementToAppear(getLocator(this, "getDismissPopupButton"), 1000, 120, getDriver());
 			getDismissPopupButton().click();
 		} catch (Exception e) {
 			LOGGER.info("Popup not found, moving on");
