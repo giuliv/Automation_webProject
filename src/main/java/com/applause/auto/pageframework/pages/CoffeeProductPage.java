b9b97cd81d4e7bf5395fabc73b586256769cc8ba
Eugen Degtyarev <ujnatg@gmail.com>
2019-03-23 17:49:28 +0200

    PEET-53-54-55-56

--- src/main/java/com/applause/auto/pageframework/pages/CoffeeProductPage.java
+++ src/main/java/com/applause/auto/pageframework/pages/CoffeeProductPage.java
@@ -51,6 +51,7 @@ public class CoffeeProductPage extends AbstractPage {
 	 */
 	public MiniCartContainerChunk clickAddToCart() {
 		LOGGER.info("Tap on Shop Coffee Button");
+		WebHelper.waitForElementToBeClickable(getAddToCartButton().getWebElement());
 		getAddToCartButton().click();
 		waitForAddingToCartSpinner();
 		return ChunkFactory.create(MiniCartContainerChunk.class, this, "");
