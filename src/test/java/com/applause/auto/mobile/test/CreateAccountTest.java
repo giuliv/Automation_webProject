48818e4487475db4e7a371c12c281a7c245053a3
Eugen Degtyarev <ujnatg@gmail.com>
2019-04-05 01:54:33 +0300

    PEET-85

---
+++ src/test/java/com/applause/auto/mobile/test/CreateAccountTest.java
@@ -0,0 +1,54 @@
+package com.applause.auto.mobile.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.applause.auto.framework.pageframework.device.DeviceViewFactory;
+import com.applause.auto.framework.pageframework.util.logger.LogController;
+import com.applause.auto.pageframework.helpers.MobileHelper;
+import com.applause.auto.pageframework.testdata.TestConstants;
+import com.applause.auto.pageframework.views.CreateAccountView;
+import com.applause.auto.pageframework.views.LandingView;
+import com.applause.auto.pageframework.views.PrivacyPolicyView;
+import com.applause.auto.pageframework.views.TermsAndConditionsView;
+
+public class CreateAccountTest extends BaseTest {
+
+	private LogController LOGGER = new LogController(CreateAccountTest.class);
+
+	@Test(groups = { TestConstants.TestNGGroups.ONBOARDING }, description = "625879")
+	public void footerLinksTest() {
+
+		LOGGER.info("Launch the app and arrive at the first onboarding screen view");
+		LandingView landingView = DeviceViewFactory.create(LandingView.class);
+		Assert.assertEquals(landingView.getHeadingTextValue(), "Earn Rewards.",
+				"First screen text value is not correct");
+
+		LOGGER.info("Tap Create Account");
+		CreateAccountView createAccountView = landingView.createAccount();
+
+		LOGGER.info("Scroll down and check the footer links");
+		MobileHelper.scrollDown(10);
+
+		LOGGER.info(
+				"Make sure above the create account button is the copy and check-box: I agree to the Privacy Policy and Terms & Conditions");
+
+		LOGGER.info("Tap on Privacy Policy link");
+		PrivacyPolicyView privacyPolicyView = createAccountView.privacyPolicy();
+
+		LOGGER.info("Make sure user is taken to Privacy Policy screen");
+		Assert.assertNotNull(privacyPolicyView, "Privacy Policy does not displayed");
+
+		LOGGER.info("Tap at top left \"Peet's\" / close browser and open Peet's to return to the app");
+		driver.activateApp("com.wearehathway.peets-dev");
+
+		LOGGER.info("Tap on the Terms and Conditions link");
+		TermsAndConditionsView termsAndConditionsView = createAccountView.termsAndConditions();
+
+		LOGGER.info("Make sure user is taken to Terms and Conditions screen");
+		Assert.assertNotNull(termsAndConditionsView, "Terms And Conditions does not displayed");
+
+		LOGGER.info("Tap at top left \"Peet's\" / close browser and open Peet's to return to the app");
+
+	}
+}
