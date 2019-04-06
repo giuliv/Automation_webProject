b8bc62310557ee05109ea14b71c62d6d3b8d799d
Eugen Degtyarev <ujnatg@gmail.com>
2019-04-06 04:16:08 +0300

    PEET-70

--- src/test/java/com/applause/auto/test/MyAccountTest.java
+++ src/test/java/com/applause/auto/test/MyAccountTest.java
@@ -4,11 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.applause.auto.framework.pageframework.util.drivers.BrowserType;
+import com.applause.auto.pageframework.chunks.MiniCartContainerChunk;
+import com.applause.auto.pageframework.chunks.VerifyYourAddressDetailsChunk;
 import com.applause.auto.pageframework.helpers.WebHelper;
 import com.applause.auto.pageframework.pages.AddBillingAddressPage;
 import com.applause.auto.pageframework.pages.AddShippingAddressPage;
 import com.applause.auto.pageframework.pages.AddressBookPage;
 import com.applause.auto.pageframework.pages.AssociateNewCardPage;
+import com.applause.auto.pageframework.pages.CheckoutConfirmationPage;
+import com.applause.auto.pageframework.pages.CheckoutPage;
+import com.applause.auto.pageframework.pages.CheckoutPaymentMethodPage;
+import com.applause.auto.pageframework.pages.CheckoutPlaceOrderPage;
+import com.applause.auto.pageframework.pages.CheckoutShippingInfoPage;
+import com.applause.auto.pageframework.pages.CoffeeProductPage;
 import com.applause.auto.pageframework.pages.EditAccountInformationPage;
 import com.applause.auto.pageframework.pages.EditBillingAddressPage;
 import com.applause.auto.pageframework.pages.EditPaymentMethodPage;
@@ -21,6 +29,7 @@ import com.applause.auto.pageframework.pages.MyAccountOrderDetailPage;
 import com.applause.auto.pageframework.pages.MyAccountPage;
 import com.applause.auto.pageframework.pages.MyAccountPeetsCardPage;
 import com.applause.auto.pageframework.pages.PaymentMethodsPage;
+import com.applause.auto.pageframework.pages.ShopCoffeePage;
 import com.applause.auto.pageframework.pages.SignInPage;
 import com.applause.auto.pageframework.pages.SignUpPage;
 import com.applause.auto.pageframework.testdata.TestConstants;
@@ -383,5 +392,53 @@ public class MyAccountTest extends BaseTest {
 
 		LOGGER.info("Verify user is logged in");
 		Assert.assertNotNull(myAccountPage, "User does not signed in");
+	}
+
+	@Test(groups = { TestConstants.TestNGGroups.MY_ACCOUNT }, description = "627699")
+	public void myAccountCreateNewAccountAfterGuestCheckoutTest() {
+		LOGGER.info("1. Navigate to landing page");
+		LandingPage landingPage = navigateToLandingPage();
+		Assert.assertNotNull(landingPage, "Failed to navigate to the landing page.");
+
+		LOGGER.info("2. Select a coffee from grid view and add to cart");
+		ShopCoffeePage shopCoffeePage = landingPage.clickShopCoffeeButton();
+		CoffeeProductPage coffeeProductPage = shopCoffeePage.clickProductName(TestConstants.TestData.COFFEE_BRAND_NAME);
+		coffeeProductPage.selectAGrind(TestConstants.TestData.GRIND);
+		MiniCartContainerChunk miniCartContainer = coffeeProductPage.clickAddToCart();
+
+		LOGGER.info("3. Select 'Proceed to Checkout'");
+		CheckoutPage checkoutPage = miniCartContainer.clickCheckout();
+		CheckoutShippingInfoPage shippingInfoPage = checkoutPage.clickContinueAsGuest();
+
+		LOGGER.info("4. Complete Contact Information");
+		VerifyYourAddressDetailsChunk verifyAddressChunk = shippingInfoPage.continueAfterFillingRequiredContactInfo();
+		shippingInfoPage = verifyAddressChunk.clickEnteredAddressButton();
+
+		LOGGER.info("5. Select ground shipping");
+		CheckoutPaymentMethodPage paymentMethodPage = shippingInfoPage
+				.setShippingMethod(TestConstants.TestData.SHIPPING_METHOD_GROUND);
+
+		LOGGER.info("6. Use credit card for payment");
+		CheckoutPlaceOrderPage placeOrderPage = paymentMethodPage.continueAfterFillingRequiredBillingInfo();
+
+		LOGGER.info("7. Click 'Place Order'");
+		CheckoutConfirmationPage confirmationPage = placeOrderPage.placeOrder();
+
+		LOGGER.info("Verify Confirmation page is displayed");
+		Assert.assertTrue(confirmationPage.getConfirmationMessage().contains("THANK YOU FOR YOUR PURCHASE!"),
+				"Order was not placed");
+
+		LOGGER.info("Order Placed: " + confirmationPage.getOrderNumber());
+
+		LOGGER.info(
+				"On Order confirmation page, create a new account for new customers. Enter email address, password, confirm password. ");
+		confirmationPage.enterPassword(TestConstants.MyAccountTestData.PASSWORD);
+		confirmationPage.enterConfirmPassword(TestConstants.MyAccountTestData.PASSWORD);
+
+		LOGGER.info("Select Create Account");
+		MyAccountPage myAccountPage = confirmationPage.createAccount();
+
+		LOGGER.info("Verify account was created");
+		Assert.assertNotNull(myAccountPage, "My Account page not found, account does not created");
 	}
 }
