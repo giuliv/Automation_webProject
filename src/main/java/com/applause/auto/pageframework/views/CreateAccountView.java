aa81a850f99adc2753360c72e1e1c59748c03ea6
Eugen Degtyarev <ujnatg@gmail.com>
2019-04-19 12:16:37 +0300

    fixup

--- src/main/java/com/applause/auto/pageframework/views/CreateAccountView.java
+++ src/main/java/com/applause/auto/pageframework/views/CreateAccountView.java
@@ -390,17 +390,17 @@ public class CreateAccountView extends AbstractDeviceView {
 		return new Button(getLocator(this, "getHideConfirmationPasswordButton"));
 	}
 
-	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[1]/android.widget.EditText", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[2]")
+	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[1]", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[2]")
 	protected PickerWheel getDOBDayPickerWheel() {
 		return new PickerWheel(getLocator(this, "getDOBDayPickerWheel"));
 	}
 
-	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[2]/android.widget.EditText", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[1]")
+	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[2]", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[1]")
 	protected PickerWheel getDOBMonthPickerWheel() {
 		return new PickerWheel(getLocator(this, "getDOBMonthPickerWheel"));
 	}
 
-	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[3]/android.widget.EditText", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[3]")
+	@MobileElementLocator(android = "//*[@resource-id='android:id/pickers']/android.widget.NumberPicker[3]", iOS = "//XCUIElementTypeDatePicker/XCUIElementTypeOther/XCUIElementTypePickerWheel[3]")
 	protected PickerWheel getDOBYearPickerWheel() {
 		return new PickerWheel(getLocator(this, "getDOBYearPickerWheel"));
 	}
