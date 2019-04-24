1037031ad4a161b364faa5dc07f5175929c8e342
Eugen Degtyarev <ujnatg@gmail.com>
2019-04-24 18:40:57 +0300

    fixup

--- src/test/java/com/applause/auto/mobile/test/BaseTest.java
+++ src/test/java/com/applause/auto/mobile/test/BaseTest.java
@@ -59,7 +59,11 @@ public class BaseTest {
 	 */
 	@AfterMethod(alwaysRun = true)
 	public void afterMethod() {
-		driver.quit();
+		try {
+			driver.quit();
+		} catch (Throwable th) {
+			LOGGER.error("Something happened during driver session release");
+		}
 		DriverWrapperManager.getInstance().deregisterDriver(driverWrapper);
 		LOGGER.info("Test case teardown complete.");
 	}
