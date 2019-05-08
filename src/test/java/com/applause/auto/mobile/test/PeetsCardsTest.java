d194cb388c37ac2791844f6a48942b425f7299b1
Eugen Degtyarev <ujnatg@gmail.com>
2019-05-08 18:48:31 +0200

    PEET-108 initial

--- src/test/java/com/applause/auto/mobile/test/PeetsCardsTest.java
+++ src/test/java/com/applause/auto/mobile/test/PeetsCardsTest.java
@@ -11,6 +11,7 @@ import com.applause.auto.framework.pageframework.device.DeviceViewFactory;
 import com.applause.auto.framework.pageframework.util.logger.LogController;
 import com.applause.auto.pageframework.testdata.TestConstants;
 import com.applause.auto.pageframework.views.AccountHistoryView;
+import com.applause.auto.pageframework.views.CheckInView;
 import com.applause.auto.pageframework.views.DashboardView;
 import com.applause.auto.pageframework.views.LandingView;
 import com.applause.auto.pageframework.views.PaymentMethodsView;
@@ -65,6 +66,62 @@ public class PeetsCardsTest extends BaseTest {
 
 		LOGGER.info("Check account history");
 		dashboardView = peetsCardsView.getBottomNavigationMenu().home();
+		AccountHistoryView accountHistory = dashboardView.getAccountProfileMenu().accountHistory();
+
+		LOGGER.info("Make sure it shows Peet's Card transaction details:\n" + "\n" + "* Peet's Card Load + $25.00\n"
+				+ "\n" + "* Date [Month Day, Year]\n" + "\n");
+		Assert.assertEquals(accountHistory.getTransactionDate(0),
+				new SimpleDateFormat("MMM d, yyyy").format(new Date()), "Incorrect transaction date");
+		Assert.assertEquals(accountHistory.getTransactionAmount(0).replace(" ", ""),
+				"+$" + new DecimalFormat("0.00").format(cardAmount), "Incorrect transaction amount");
+
+	}
+
+	@Test(groups = { TestConstants.TestNGGroups.PEETS_CARDS }, description = "625916")
+	public void reloadDigitalCard25SavedCC() {
+		LOGGER.info("Launch the app and arrive at the first on boarding screen view");
+		LandingView landingView = DeviceViewFactory.create(LandingView.class);
+
+		DashboardView dashboardView = peetsMobileHelper.signIn(landingView, TestConstants.MyAccountTestData.EMAIL,
+				TestConstants.MyAccountTestData.PASSWORD, DashboardView.class);
+		Assert.assertNotNull(dashboardView, "Dashboard View does not displayed");
+
+		LOGGER.info("Tap Check In icon on bottom nav bar");
+		CheckInView checkInView = dashboardView.getBottomNavigationMenu().checkIn();
+
+		LOGGER.info("User should be taken to check in screen");
+		Assert.assertNotNull(checkInView, "User does not taken to Check In view");
+		int oldBalance = Integer.parseInt(checkInView.getBalance());
+
+		LOGGER.info("Tap Add Value button from Peet's Card Balance tile");
+		checkInView.addValue();
+
+		LOGGER.info("Tap pencil icon to view Payment Method screen");
+		PaymentMethodsView paymentMethodsView = checkInView.edit();
+
+		LOGGER.info("User should be taken to payment method screen");
+		Assert.assertNotNull(paymentMethodsView, "User does not taken to payment method view");
+
+		LOGGER.info("Select a saved credit card");
+		checkInView = paymentMethodsView.clickSavedPaymentMethod2(CheckInView.class);
+
+		LOGGER.info("User should return to add value to my peet's card screen");
+		Assert.assertNotNull(paymentMethodsView, "User does taken to Peets Cards screen");
+
+		LOGGER.info("$25 tile should be default highlighted");
+		int cardAmount = 25;
+		Assert.assertTrue(checkInView.isAmountSelected("$" + cardAmount), "$25 tile does not highlighted");
+
+		LOGGER.info("Tap Confirm Value button");
+		checkInView = checkInView.confirm();
+		int newBalance = Integer.parseInt(checkInView.getBalance());
+
+		LOGGER.info(
+				"Make sure user is able to successfully add value to card and peet's card screen shows card balance of $25.00");
+		Assert.assertEquals(newBalance - oldBalance, cardAmount, "Balance does not changed properly");
+
+		LOGGER.info("Check account history");
+		dashboardView = checkInView.getBottomNavigationMenu().home();
 		AccountHistoryView accountHistory = dashboardView.getAccountProfileMenu().accountHistory();
 
 		LOGGER.info("Make sure it shows Peet's Card transaction details:\n" + "\n" + "* Peet's Card Load + $25.00\n"
