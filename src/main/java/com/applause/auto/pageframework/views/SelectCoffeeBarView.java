cd4fc85a66d200b33353889fc151cd427a392ede
Eugen Degtyarev <ujnatg@gmail.com>
2019-06-18 20:20:05 +0300

    PEET-123

---
+++ src/main/java/com/applause/auto/pageframework/views/SelectCoffeeBarView.java
@@ -0,0 +1,87 @@
+package com.applause.auto.pageframework.views;
+
+import java.lang.invoke.MethodHandles;
+
+import com.applause.auto.framework.pageframework.device.AbstractDeviceView;
+import com.applause.auto.framework.pageframework.device.DeviceChunkFactory;
+import com.applause.auto.framework.pageframework.device.MobileElementLocator;
+import com.applause.auto.framework.pageframework.device.factory.AndroidImplementation;
+import com.applause.auto.framework.pageframework.device.factory.IosImplementation;
+import com.applause.auto.framework.pageframework.devicecontrols.BaseDeviceControl;
+import com.applause.auto.framework.pageframework.devicecontrols.Button;
+import com.applause.auto.framework.pageframework.devicecontrols.Text;
+import com.applause.auto.framework.pageframework.util.logger.LogController;
+import com.applause.auto.pageframework.chunks.mobile.AllowLocationServicesPopupChunk;
+
+@AndroidImplementation(AndroidSelectCoffeeBarView.class)
+@IosImplementation(SelectCoffeeBarView.class)
+public class SelectCoffeeBarView extends AbstractDeviceView {
+
+	protected final static LogController LOGGER = new LogController(MethodHandles.lookup().getClass());
+
+	@Override
+	protected void waitUntilVisible() {
+		syncHelper.waitForElementToAppear(getSignature());
+	}
+
+	/*
+	 * Public Actions
+	 */
+
+	/**
+	 * Gets enable location description.
+	 *
+	 * @return the enable location description
+	 */
+	public String getEnableLocationDescription() {
+		return getEnableLocationDescriptionText().getStringValue();
+	}
+
+	/**
+	 * Is enable location button displayed boolean.
+	 *
+	 * @return the boolean
+	 */
+	public boolean isEnableLocationButtonDisplayed() {
+		return syncHelper.isElementDisplayed(getLocator(this, "getEnableLocationButton"));
+	}
+
+	/**
+	 * Enable location allow location services popup chunk.
+	 *
+	 * @return the allow location services popup chunk
+	 */
+	public AllowLocationServicesPopupChunk enableLocation() {
+		LOGGER.info("Tap enable location button");
+		getEnableLocationButton().pressButton();
+		return DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "");
+	}
+
+	/*
+	 * Protected Getters
+	 */
+
+	@MobileElementLocator(android = "//android.widget.TextView[@resource-id='Select Coffeebar']", iOS = "//XCUIElementTypeNavigationBar[@name='Select Coffeebar']")
+	protected BaseDeviceControl getSignature() {
+		return new BaseDeviceControl(getLocator(this, "getSignature"));
+	}
+
+	@MobileElementLocator(android = "com.wearehathway.peets.development:id/noStoreMessage", iOS = "//XCUIElementTypeButton[@name=\"Enable Location\"]/preceding-sibling::*")
+	protected Text getEnableLocationDescriptionText() {
+		return new Text(getLocator(this, "getEnableLocationDescriptionText"));
+	}
+
+	@MobileElementLocator(android = "com.wearehathway.peets.development:id/enableLocationServiceButton", iOS = "Enable Location")
+	protected Button getEnableLocationButton() {
+		return new Button(getLocator(this, "getEnableLocationButton"));
+	}
+
+}
+
+class AndroidSelectCoffeeBarView extends SelectCoffeeBarView {
+	@Override
+	protected void waitUntilVisible() {
+		DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "").notNow();
+		syncHelper.waitForElementToAppear(getSignature());
+	}
+}
---
+++ src/test/java/com/applause/auto/mobile/test/OrderAheadTest.java
@@ -0,0 +1,104 @@
+package com.applause.auto.mobile.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.applause.auto.framework.pageframework.device.DeviceViewFactory;
+import com.applause.auto.framework.pageframework.util.logger.LogController;
+import com.applause.auto.pageframework.chunks.mobile.AllowLocationServicesPopupChunk;
+import com.applause.auto.pageframework.chunks.mobile.AllowLocationServicesSystemPopupChunk;
+import com.applause.auto.pageframework.testdata.TestConstants;
+import com.applause.auto.pageframework.views.DashboardView;
+import com.applause.auto.pageframework.views.LandingView;
+import com.applause.auto.pageframework.views.OrderAheadView;
+import com.applause.auto.pageframework.views.SelectCoffeeBarView;
+
+public class OrderAheadTest extends BaseTest {
+
+	private LogController LOGGER = new LogController(OrderAheadTest.class);
+
+	@Test(groups = { TestConstants.TestNGGroups.ORDER_AHEAD }, description = "625889")
+	public void locationServicesNotEnabled() {
+		LOGGER.info("Launch the app and arrive at the first on boarding screen view");
+		LandingView landingView = DeviceViewFactory.create(LandingView.class);
+		DashboardView dashboardView = peetsMobileHelper.signIn(landingView, TestConstants.MyAccountTestData.EMAIL,
+				TestConstants.MyAccountTestData.PASSWORD, DashboardView.class);
+		Assert.assertNotNull(dashboardView, "Dashboard View does not displayed");
+
+		LOGGER.info("Tap Order icon on the bottom nav bar");
+		OrderAheadView orderAhead = dashboardView.getBottomNavigationMenu().order();
+
+		LOGGER.info("Header: Order Ahead");
+		Assert.assertEquals(orderAhead.getHeadingTextValue(), "Order Ahead", "Incorrect header");
+
+		LOGGER.info("Sub-header: Bypass the line and proceed to great coffee.");
+		Assert.assertEquals(orderAhead.getSubHeaderTextValue(), "Bypass the line and proceed to great coffee.",
+				"Incorrect sub-header");
+
+		LOGGER.info("[Button] See Participating Coffeebars");
+		Assert.assertTrue(orderAhead.isParticipatingCoffeebarsDisplayed(),
+				"See Participating Coffeebars does not displayed");
+
+		LOGGER.info("Tap See Participating Coffeebars");
+		SelectCoffeeBarView selectCoffeeBarView = orderAhead.participatingCoffeebars();
+
+		LOGGER.info("User should be taken to Select Coffeebar screen:");
+		Assert.assertNotNull(selectCoffeeBarView, "User does not taken to Select Coffeebar screen");
+
+		LOGGER.info("Allow Location Services to help you find nearby Peet's Coffeebars.");
+		Assert.assertEquals(selectCoffeeBarView.getEnableLocationDescription(),
+				"Allow Location Services to help you find nearby Peet's Coffeebars.",
+				"'Allow Location Services to help you find nearby Peet's Coffeebars.' text does not displayed");
+
+		LOGGER.info("[Button] Enable Location");
+		Assert.assertTrue(selectCoffeeBarView.isEnableLocationButtonDisplayed(),
+				"[Button] Enable Location does not displayed");
+
+		LOGGER.info("Tap enable location");
+		AllowLocationServicesPopupChunk allowLocationServicesPopupChunk = selectCoffeeBarView.enableLocation();
+
+		LOGGER.info("Make sure Peet's branded Location Services alert appears:");
+		LOGGER.info("Title: Allow Location Services to help you find nearby Peet's Coffeebars.");
+		Assert.assertEquals(allowLocationServicesPopupChunk.getTitle(),
+				"Allow Location Services to help you find nearby Peet’s Coffeebars.",
+				"'Allow Location Services to help you find nearby Peet’s Coffeebars.' title does not found");
+
+		LOGGER.info("Text: Location Services will:\n" + "\n" + "* Only use your location while using the app\n" + "\n"
+				+ "* Not share your locations or information\n" + "\n" + "* Pinpoint the coffeebars closest to you");
+		Assert.assertEquals(allowLocationServicesPopupChunk.getFormattedMessage(),
+				"Location Services will: Allow Location Services to help you find nearby Peet’s Coffeebars. Only use your location while using the app Not share your locations or information Pinpoint the coffeebars closest to you",
+				"Unexpected text: ");
+		LOGGER.info("[Button] Not Now [Button] Allow");
+		Assert.assertTrue(allowLocationServicesPopupChunk.isAllowButtonDisplayed(), "Allow button does not displayed");
+		Assert.assertTrue(allowLocationServicesPopupChunk.isNotNowButtonDisplayed(),
+				"Not Now button does not displayed");
+
+		LOGGER.info("Tap Allow");
+		AllowLocationServicesSystemPopupChunk allowLocationServicesSystemPopupChunk = allowLocationServicesPopupChunk
+				.allow();
+
+		LOGGER.info("Make sure user sees another UI alert:\n" + "\n"
+				+ "Title: Allow \"Peet's\" to access your location while you are using the app?");
+		Assert.assertEquals(allowLocationServicesSystemPopupChunk.getTitle(),
+				"Allow “Peet's” to access your location while you are using the app?",
+				"Title: Allow \"Peet's\" to access your location while you are using the app? doe not displayed");
+		LOGGER.info(
+				"Text: Location services will only use your location while using the app, and will not share your location or information. Your location will be used to find the nearest coffeebar and place a mobile order.");
+		Assert.assertEquals(allowLocationServicesSystemPopupChunk.getFormattedMessage(),
+				"Location Services will only use your location while using the app, and will not share your location or information. Your location will be used to find the nearest coffeebar and place a mobile order.",
+				"Text: 'Location Services will only use your location while using the app, and will not share your location or information. Your location will be used to find the nearest coffeebar and place a mobile order.' does not displayed");
+
+		LOGGER.info("[Don't Alllow] [Allow]");
+		Assert.assertTrue(allowLocationServicesSystemPopupChunk.isAllowButtonDisplayed(),
+				"Allow button does not displayed");
+		Assert.assertTrue(allowLocationServicesSystemPopupChunk.isDoNotAllowButtonDisplayed(),
+				"Don't Allow button does not displayed");
+
+		LOGGER.info("Tap Allow");
+		allowLocationServicesSystemPopupChunk.allow();
+
+		LOGGER.info(
+				"User should see loading dial, nearby stores should appear under nearby stores tab and user should be able to select a store");
+	}
+
+}
