6764e5681574f18be29e8f263e4eea3d97230f44
Drew Horn <ahorn@applause.com>
2019-08-09 14:23:36 -0500

    Clean up the custom WebHelper

--- src/main/java/com/applause/auto/web/helpers/WebHelper.java
+++ src/main/java/com/applause/auto/web/helpers/WebHelper.java
@@ -3,23 +3,15 @@ package com.applause.auto.web.helpers;
 import java.lang.invoke.MethodHandles;
 import java.util.Date;
 
-import org.apache.log4j.Logger;
+import com.applause.auto.util.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import com.applause.auto.framework.pageframework.util.TestHelper;
-import com.applause.auto.framework.pageframework.util.drivers.DriverWrapperManager;
 
 public class WebHelper {
-	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().getClass());
 
-	private static WebDriver getDriver() {
-		return (WebDriver) DriverWrapperManager.getInstance().getPrimaryDriverWrapper().getDriver();
-	}
+	private static final Logger logger = LogManager.getLogger(MethodHandles.lookup().getClass());
 
 	/**
 	 * Returns a unique value based on given name and timestamp
@@ -50,7 +42,7 @@ public class WebHelper {
 	 * @param webElement
 	 */
 	public void jsClick(final WebElement webElement) {
-		final JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+		final JavascriptExecutor executor = (JavascriptExecutor) DriverManager.getDriver();
 		executor.executeScript("arguments[0].click();", webElement);
 	}
 
@@ -63,7 +55,7 @@ public class WebHelper {
 	 *            as text
 	 */
 	public void jsSelect(WebElement element, String item) {
-		JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+		JavascriptExecutor executor = (JavascriptExecutor) DriverManager.getDriver();
 		executor.executeScript(
 				"const textToFind = '" + item + "';" + "const dd = arguments[0];"
 						+ "dd.selectedIndex = [...dd.options].findIndex (option => option.text === textToFind);",
@@ -79,7 +71,7 @@ public class WebHelper {
 	 *            as text
 	 */
 	public void jsSelectByContainedText(WebElement element, String text) {
-		JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+		JavascriptExecutor executor = (JavascriptExecutor) DriverManager.getDriver();
 		executor.executeScript(
 				"const textToFind = '" + text + "';" + "const dd = arguments[0];"
 						+ "dd.selectedIndex = [...dd.options].findIndex (option => option.text.includes(textToFind));",
@@ -95,7 +87,7 @@ public class WebHelper {
 	 *            as value
 	 */
 	public void jsSelectByValue(WebElement element, String value) {
-		JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+		JavascriptExecutor executor = (JavascriptExecutor) DriverManager.getDriver();
 		executor.executeScript(
 				"const valueToFind = '" + value + "';" + "const dd = arguments[0];"
 						+ "dd.selectedIndex = [...dd.options].findIndex (option => option.value === valueToFind);",
@@ -109,17 +101,16 @@ public class WebHelper {
 	 *            the y offset
 	 */
 	public static void shiftWindowView(final int yOffset) {
-		final JavascriptExecutor executor = (JavascriptExecutor) getDriver();
+		final JavascriptExecutor executor = (JavascriptExecutor) DriverManager.getDriver();
 		executor.executeScript(String.format("window.scrollBy(0, %s)", yOffset), "");
 	}
 
-	public static WebElement waitForElementToBeClickable(WebElement element) {
-		WebDriverWait wait = new WebDriverWait(getDriver(), 10);
-		return wait.until(ExpectedConditions.elementToBeClickable(element));
-	}
-
+	/**
+	 * Scrolls to the given web element
+	 * @param element
+	 */
 	public static void scrollToElement(WebElement element) {
-		JavascriptExecutor jse = (JavascriptExecutor) getDriver();
+		JavascriptExecutor jse = (JavascriptExecutor) DriverManager.getDriver();;
 		long windowHeight = (long) jse.executeScript("return window.innerHeight");
 
 		// scrolls element to view...
@@ -128,38 +119,5 @@ public class WebHelper {
 		// ... and then scroll half-way up the page, to ensure
 		// the element is not obscured by another element.
 		jse.executeScript("window.scrollBy(0, " + -(windowHeight / 2) + ")", "");
-	}
-
-	public static void waitForLoad() {
-		LOGGER.debug("======> Wait for page load");
-		long startTime = TestHelper.getCurrentGMT6Time();
-
-		try {
-			waitForDocument();
-			// waitForJquery(120);
-		} catch (Throwable t) {
-			LOGGER.warn(t.getMessage());
-			LOGGER.warn("Page not loaded properly. Continue....");
-		}
-		LOGGER.debug("======> Wait for page load comleted in: " + (TestHelper.getCurrentGMT6Time() - startTime));
-	}
-
-	public static void waitForJquery(int seconds) {
-		LOGGER.info("jQuery synchronization wait for " + seconds + " seconds");
-		long start = System.currentTimeMillis();
-		long end = start + seconds * 1000;
-		boolean exitCond = false;
-		while ((System.currentTimeMillis() < end) && !exitCond) {
-			try {
-				exitCond = (boolean) ((JavascriptExecutor) getDriver())
-						.executeScript("return !!window.$ && window.$.active == 0", new Object[0]);
-				LOGGER.info("waitForJquery script returned " + exitCond);
-			} catch (WebDriverException we) {
-				LOGGER.info("jQuery throw exception. Status undefined: " + we.getMessage());
-			}
-		}
-		if (!exitCond) {
-			LOGGER.info("waitForDocument timed out.");
-		}
 	}
 }
