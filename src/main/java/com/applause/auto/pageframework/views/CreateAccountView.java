ca32afec02934127528eb9832fc8c92552e25ab5
Eugen Degtyarev <ujnatg@gmail.com>
2019-09-11 02:04:11 +0300

    PEET-145

--- src/main/java/com/applause/auto/pageframework/views/CreateAccountView.java
+++ src/main/java/com/applause/auto/pageframework/views/CreateAccountView.java
@@ -461,7 +461,7 @@ public class CreateAccountView extends AbstractDeviceView {
 		return new Checkbox(getLocator(this, "getEmailsWithOffersCheckBox"));
 	}
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/agreePrivacyPolicyCheckBox", iOS = "//XCUIElementTypeTextView[contains(@value,'I agree to the Privacy Policy and Terms']/following-sibling::XCUIElementTypeButton")
+	@MobileElementLocator(android = "com.wearehathway.peets.development:id/agreePrivacyPolicyCheckBox", iOS = "//XCUIElementTypeTextView[contains(@value,'I agree to the Privacy Policy and Terms')]/following-sibling::XCUIElementTypeButton")
 	protected Checkbox getAgreePrivacyPolicyAndTermsAndConditions() {
 		return new Checkbox(getLocator(this, "getAgreePrivacyPolicyAndTermsAndConditions"));
 	}
