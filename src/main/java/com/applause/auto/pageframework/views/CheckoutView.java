3af258477d4d5d501ced109d01baf7d60ee3cecb
Eugen Degtyarev <ujnatg@gmail.com>
2019-10-10 16:25:12 +0300

    (PEET-144)

---
+++ src/main/java/com/applause/auto/pageframework/views/CheckoutView.java
@@ -0,0 +1,48 @@
+package com.applause.auto.pageframework.views;
+
+import com.applause.auto.framework.pageframework.device.AbstractDeviceView;
+import com.applause.auto.framework.pageframework.device.DeviceViewFactory;
+import com.applause.auto.framework.pageframework.device.MobileElementLocator;
+import com.applause.auto.framework.pageframework.device.factory.AndroidImplementation;
+import com.applause.auto.framework.pageframework.device.factory.IosImplementation;
+import com.applause.auto.framework.pageframework.devicecontrols.Button;
+import com.applause.auto.framework.pageframework.devicecontrols.Text;
+import com.applause.auto.framework.pageframework.util.logger.LogController;
+import com.applause.auto.pageframework.helpers.MobileHelper;
+
+import java.lang.invoke.MethodHandles;
+
+@AndroidImplementation(CheckoutView.class)
+@IosImplementation(CheckoutView.class)
+public class CheckoutView extends AbstractDeviceView {
+
+    protected final static LogController LOGGER = new LogController(MethodHandles.lookup().getClass());
+
+    @Override
+    protected void waitUntilVisible() {
+        syncHelper.waitForElementToAppear(getHeadingText(), 120000);
+    }
+
+
+
+    /*
+     * Protected Getters
+     */
+
+    @MobileElementLocator(android = "//android.widget.TextView[@text='CHECKOUT']", iOS = "//XCUIElementTypeNavigationBar[@name=\"CHECKOUT\"]")
+    protected Text getHeadingText() {
+        return new Text(getLocator(this, "getHeadingText"));
+    }
+
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/checkoutButton", iOS = "Place Order")
+    protected Button getPlaceOrderButton() {
+        return new Button(getLocator(this, "getPlaceOrderButton"));
+    }
+
+    public <T extends AbstractDeviceView> T placeOrder(Class<T> clazz) {
+        LOGGER.info("Tap place order");
+        MobileHelper.scrollDownToElement(getLocator(this, "getPlaceOrderButton"), 5);
+        getPlaceOrderButton().pressButton();
+        return DeviceViewFactory.create(clazz);
+    }
+}
--- src/main/java/com/applause/auto/pageframework/views/SelectCoffeeBarView.java
+++ src/main/java/com/applause/auto/pageframework/views/SelectCoffeeBarView.java
@@ -1,6 +1,6 @@
 package com.applause.auto.pageframework.views;
 
-import java.lang.invoke.MethodHandles;
+import com.google.common.collect.ImmutableMap;
 
 import com.applause.auto.framework.pageframework.device.AbstractDeviceView;
 import com.applause.auto.framework.pageframework.device.DeviceChunkFactory;
@@ -15,141 +15,152 @@ import com.applause.auto.framework.pageframework.devicecontrols.TextBox;
 import com.applause.auto.framework.pageframework.util.logger.LogController;
 import com.applause.auto.pageframework.chunks.mobile.AllowLocationServicesPopupChunk;
 
-import com.google.common.collect.ImmutableMap;
+import java.lang.invoke.MethodHandles;
 
 @AndroidImplementation(AndroidSelectCoffeeBarView.class)
 @IosImplementation(SelectCoffeeBarView.class)
 public class SelectCoffeeBarView extends AbstractDeviceView {
 
-	protected final static LogController LOGGER = new LogController(MethodHandles.lookup().getClass());
+    protected final static LogController LOGGER = new LogController(MethodHandles.lookup().getClass());
 
-	@Override
-	protected void waitUntilVisible() {
-		syncHelper.waitForElementToAppear(getSignature());
-	}
+    @Override
+    protected void waitUntilVisible() {
+        if (!getSignature().exists()) {
+            throw new AssertionError("Select coffeebar view not displayed");
+        }
+    }
 
-	/*
-	 * Public Actions
-	 */
+    /*
+     * Public Actions
+     */
 
-	/**
-	 * Gets enable location description.
-	 *
-	 * @return the enable location description
-	 */
-	public String getEnableLocationDescription() {
-		return getEnableLocationDescriptionText().getStringValue();
-	}
+    /**
+     * Gets enable location description.
+     *
+     * @return the enable location description
+     */
+    public String getEnableLocationDescription() {
+        return getEnableLocationDescriptionText().getStringValue();
+    }
 
-	/**
-	 * Is enable location button displayed boolean.
-	 *
-	 * @return the boolean
-	 */
-	public boolean isEnableLocationButtonDisplayed() {
-		return syncHelper.isElementDisplayed(getLocator(this, "getEnableLocationButton"));
-	}
+    /**
+     * Is enable location button displayed boolean.
+     *
+     * @return the boolean
+     */
+    public boolean isEnableLocationButtonDisplayed() {
+        return syncHelper.isElementDisplayed(getLocator(this, "getEnableLocationButton"));
+    }
 
-	/**
-	 * Enable location allow location services popup chunk.
-	 *
-	 * @return the allow location services popup chunk
-	 */
-	public AllowLocationServicesPopupChunk enableLocation() {
-		LOGGER.info("Tap enable location button");
-		getEnableLocationButton().pressButton();
-		return DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "");
-	}
+    /**
+     * Enable location allow location services popup chunk.
+     *
+     * @return the allow location services popup chunk
+     */
+    public AllowLocationServicesPopupChunk enableLocation() {
+        LOGGER.info("Tap enable location button");
+        return DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "");
+    }
 
-	/**
-	 * Search.
-	 *
-	 * @param searchTxt
-	 *            the search txt
-	 */
-	public void search(String searchTxt) {
-		LOGGER.info("Searching for store: " + searchTxt);
-		getChangeStoreButton().pressButton();
-		getSearchButton().pressButton();
-		getSearchTextBox().enterText(searchTxt + "\n");
-	}
+    /**
+     * Search.
+     *
+     * @param searchTxt the search txt
+     */
+    public void search(String searchTxt) {
+        LOGGER.info("Searching for store: " + searchTxt);
+        try {
+            getChangeStoreButton().pressButton();
+        } catch (Throwable throwable) {
 
-	/**
-	 * Open coffeebar from search results new order view.
-	 *
-	 * @param index
-	 *            the index
-	 * @return the new order view
-	 */
-	public NewOrderView openCoffeebarFromSearchResults(int index) {
-		LOGGER.info("Tap on Search result");
-		getSearchResultText(index).tapCenterOfElement();
-		return DeviceViewFactory.create(NewOrderView.class);
-	}
+        }
+        getSearchButton().pressButton();
+        getSearchTextBox().enterText(searchTxt + "\n");
+    }
 
-	public boolean isStoresDisplayed() {
-		boolean result = queryHelper.doesElementExist(getLocator(this, "getSearchResultText", 1));
-		return result;
-	}
+    /**
+     * Open coffeebar from search results new order view.
+     *
+     * @param index the index
+     * @return the new order view
+     */
+    public NewOrderView openCoffeebarFromSearchResults(int index) {
+        LOGGER.info("Tap on Search result");
+        getSearchResultText(index).tapCenterOfElement();
+        return DeviceViewFactory.create(NewOrderView.class);
+    }
 
-	/*
-	 * Protected Getters
-	 */
+    public boolean isStoresDisplayed() {
+        boolean result = queryHelper.doesElementExist(getLocator(this, "getSearchResultText", 1));
+        return result;
+    }
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/storeContainer", iOS = "//XCUIElementTypeNavigationBar[@name='Select Coffeebar']")
-	protected BaseDeviceControl getSignature() {
-		return new BaseDeviceControl(getLocator(this, "getSignature"));
-	}
+    /*
+     * Protected Getters
+     */
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/noStoreMessage", iOS = "//XCUIElementTypeButton[@name=\"Enable Location\"]/preceding-sibling::*")
-	protected Text getEnableLocationDescriptionText() {
-		return new Text(getLocator(this, "getEnableLocationDescriptionText"));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/storeContainer", iOS = "//XCUIElementTypeOther[@name='FIND A COFFEEBAR']")
+    protected BaseDeviceControl getSignature() {
+        return new BaseDeviceControl(getLocator(this, "getSignature"));
+    }
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/enableLocationServiceButton", iOS = "Enable Location")
-	protected Button getEnableLocationButton() {
-		return new Button(getLocator(this, "getEnableLocationButton"));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/noStoreMessage", iOS = "//XCUIElementTypeButton[@name=\"Enable Location\"]/preceding-sibling::*")
+    protected Text getEnableLocationDescriptionText() {
+        return new Text(getLocator(this, "getEnableLocationDescriptionText"));
+    }
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/changeTextView", iOS = "TODO")
-	protected Button getChangeStoreButton() {
-		return new Button(getLocator(this, "getChangeStoreButton"));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/enableLocationServiceButton", iOS = "Enable Location")
+    protected Button getEnableLocationButton() {
+        return new Button(getLocator(this, "getEnableLocationButton"));
+    }
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/storeSearchIcon", iOS = "search magnifier")
-	protected Button getSearchButton() {
-		return new Button(getLocator(this, "getSearchButton"));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/changeTextView", iOS = "TODO")
+    protected Button getChangeStoreButton() {
+        return new Button(getLocator(this, "getChangeStoreButton"));
+    }
 
-	@MobileElementLocator(android = "com.wearehathway.peets.development:id/searchField", iOS = "Search for coffeebar")
-	protected TextBox getSearchTextBox() {
-		return new TextBox(getLocator(this, "getSearchTextBox"));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/storeSearchIcon", iOS = "Enter Zip or City, State")
+    protected Button getSearchButton() {
+        return new Button(getLocator(this, "getSearchButton"));
+    }
 
-	@MobileElementLocator(android = "(//android.widget.RelativeLayout[@resource-id='com.wearehathway.peets.development:id/storeDetail'])[%s]", iOS = "//XCUIElementTypeTable/XCUIElementTypeCell[%s]")
-	protected TextBox getSearchResultText(int index) {
-		return new TextBox(getLocator(this, "getSearchResultText", index));
-	}
+    @MobileElementLocator(android = "com.wearehathway.peets.development:id/searchField", iOS = "//XCUIElementTypeSearchField[@name=\"Enter Zip or City, State\"]")
+    protected TextBox getSearchTextBox() {
+        return new TextBox(getLocator(this, "getSearchTextBox"));
+    }
+
+    @MobileElementLocator(android = "(//android.widget.RelativeLayout[@resource-id='com.wearehathway.peets.development:id/storeDetail'])[%s]", iOS = "(//XCUIElementTypeButton[@name=\"Order\"])[%s]")
+    protected TextBox getSearchResultText(int index) {
+        return new TextBox(getLocator(this, "getSearchResultText", index));
+    }
 
 }
 
 class AndroidSelectCoffeeBarView extends SelectCoffeeBarView {
-	@Override
-	protected void waitUntilVisible() {
-		// Workaround for Automator hang
-		try {
-			DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "").notNow();
-		} catch (AssertionError ase) {
-			LOGGER.warn("No popup found");
-		}
-		syncHelper.waitForElementToAppear(getSignature());
-	}
+    @Override
+    protected void waitUntilVisible() {
+        // Workaround for Automator hang
+        try {
+            DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "").notNow();
+        } catch (AssertionError ase) {
+            LOGGER.warn("No popup found");
+        }
+        syncHelper.waitForElementToAppear(getSignature());
+    }
 
-	public void search(String searchTxt) {
-		LOGGER.info("Searching for store: " + searchTxt);
-		getChangeStoreButton().pressButton();
-		getSearchButton().pressButton();
-		getSearchTextBox().enterText(searchTxt);
-		getDriver().executeScript("mobile: performEditorAction", ImmutableMap.of("action", "search"));
-	}
+    public void search(String searchTxt) {
+        LOGGER.info("Searching for store: " + searchTxt);
+        getChangeStoreButton().pressButton();
+        getSearchButton().pressButton();
+        getSearchTextBox().enterText(searchTxt);
+        getDriver().executeScript("mobile: performEditorAction", ImmutableMap.of("action", "search"));
+    }
+
+    @Override
+    public AllowLocationServicesPopupChunk enableLocation() {
+        LOGGER.info("Tap enable location button");
+        getEnableLocationButton().pressButton();
+        return DeviceChunkFactory.create(AllowLocationServicesPopupChunk.class, "");
+    }
+
 }
