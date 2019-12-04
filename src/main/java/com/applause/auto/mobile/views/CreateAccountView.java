8855b299296676a182f030a43460dbd3f29102c9
Taras Lytvyn <tlytvyn@lohika.com>
2019-12-04 16:33:43 +0200

    organized imports

--- src/main/java/com/applause/auto/mobile/views/CreateAccountView.java
+++ src/main/java/com/applause/auto/mobile/views/CreateAccountView.java
@@ -20,8 +20,9 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.offset.PointOption;
-import java.time.Duration;
 import org.openqa.selenium.Dimension;
+
+import java.time.Duration;
 
 @Implementation(is = AndroidCreateAccountView.class, on = Platform.MOBILE_ANDROID)
 @Implementation(is = CreateAccountView.class, on = Platform.MOBILE_IOS)
