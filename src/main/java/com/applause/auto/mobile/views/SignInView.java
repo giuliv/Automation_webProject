06e13741685f4f8c032e760d069f71fb7b4cafea
Eugen Degtyarev <ujnatg@gmail.com>
2020-02-24 03:50:05 +0200

    (PEET-173)

--- src/main/java/com/applause/auto/mobile/views/SignInView.java
+++ src/main/java/com/applause/auto/mobile/views/SignInView.java
@@ -181,6 +181,7 @@ public class SignInView extends BaseComponent {
    */
   public <T extends BaseComponent> T signIn(Class<T> clazz) {
     logger.info("Click on Sign In button");
+    getSignInButton.initialize();
     getSignInButton.click();
     SyncHelper.wait(Until.uiElement(getLoader).notVisible());
     return ComponentFactory.create(clazz);
