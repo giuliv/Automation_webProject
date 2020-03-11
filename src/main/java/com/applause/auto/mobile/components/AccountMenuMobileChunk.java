9f37da8f8d4bad8dda89d0e9144169d04e71bc9d
George Semfield <georgesemfield@Georges-MacBook-Pro.local>
2020-03-11 14:07:58 -0700

    trying container object type

--- src/main/java/com/applause/auto/mobile/components/AccountMenuMobileChunk.java
+++ src/main/java/com/applause/auto/mobile/components/AccountMenuMobileChunk.java
@@ -13,7 +13,7 @@ import com.applause.auto.pageobjectmodel.annotation.Implementation;
 import com.applause.auto.pageobjectmodel.annotation.Locate;
 import com.applause.auto.pageobjectmodel.base.BaseComponent;
 import com.applause.auto.pageobjectmodel.elements.Button;
-import com.applause.auto.pageobjectmodel.elements.Text;
+import com.applause.auto.pageobjectmodel.elements.ContainerElement;
 import com.applause.auto.pageobjectmodel.factory.ComponentFactory;
 import com.applause.auto.util.DriverManager;
 import com.applause.auto.util.helper.SyncHelper;
@@ -90,7 +90,7 @@ public class AccountMenuMobileChunk extends BaseComponent {
 
   @Locate(xpath = "//XCUIElementTypeOther[@name=\"URL\"]", on = Platform.MOBILE_IOS)
   @Locate(id = "com.android.chrome:id/url_bar", on = Platform.MOBILE_ANDROID)
-  protected Text urlBar;
+  protected ContainerElement urlBar;
 
   @Locate(xpath = "//XCUIElementTypeButton[@name='Done']", on = Platform.MOBILE_IOS)
   protected Button getDoneButton;
