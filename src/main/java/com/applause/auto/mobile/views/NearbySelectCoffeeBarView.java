2807b0fac24c2ab8c741523bbfd4afa890a917c3
Eugen Degtyarev <ujnatg@gmail.com>
2020-08-24 03:43:29 +0300

    fixup

--- src/main/java/com/applause/auto/mobile/views/NearbySelectCoffeeBarView.java
+++ src/main/java/com/applause/auto/mobile/views/NearbySelectCoffeeBarView.java
@@ -25,7 +25,9 @@ public class NearbySelectCoffeeBarView extends BaseComponent {
 
   /* -------- Elements -------- */
 
-  @Locate(xpath = "//XCUIElementTypeButton[@name='Recents']", on = Platform.MOBILE_IOS)
+  @Locate(
+      xpath = "//XCUIElementTypeButton[@value='1' and @name='Nearby']",
+      on = Platform.MOBILE_IOS)
   @Locate(id = "com.wearehathway.peets.development:id/storeContainer", on = Platform.MOBILE_ANDROID)
   protected ContainerElement getSignature;
 
