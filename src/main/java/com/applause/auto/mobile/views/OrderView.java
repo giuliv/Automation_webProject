3547d837c80a890ce3cce587ec44ffe636609503
Eugen Degtyarev <ujnatg@gmail.com>
2020-08-24 18:05:09 +0300

    fixup

--- src/main/java/com/applause/auto/mobile/views/OrderView.java
+++ src/main/java/com/applause/auto/mobile/views/OrderView.java
@@ -26,7 +26,7 @@ public class OrderView extends BaseComponent {
   @Locate(
       className =
           "//android.widget.ImageButton[contains(@content-desc,\"Navigate up\") or contains(@content-desc,\"Nach oben\")]/following-sibling::android.widget.TextView[@text='ORDER']|"
-              + "android.widget.TextView[@text='ORDER']",
+              + "//android.widget.TextView[@text='ORDER']",
       on = Platform.MOBILE_ANDROID)
   protected Text getHeadingText;
 
