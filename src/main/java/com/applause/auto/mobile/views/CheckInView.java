a04e91d13abc355ddb40188fdbbf5aea1c197a66
Eugen Degtyarev <ujnatg@gmail.com>
2020-10-03 01:00:57 +0300

    fixup

--- src/main/java/com/applause/auto/mobile/views/CheckInView.java
+++ src/main/java/com/applause/auto/mobile/views/CheckInView.java
@@ -67,7 +67,8 @@ public class CheckInView extends BaseComponent {
   public void addValue() {
     logger.info("Tap on Add Value");
     SyncHelper.sleep(20000);
-    while (getAddValueButton.exists() && getAddValueButton.isDisplayed()) {
+    int retryCounter = 0;
+    while (getAddValueButton.exists() && getAddValueButton.isDisplayed() && retryCounter++ < 5) {
       MobileHelper.tapByCoordinatesOnElementCenter(getAddValueButton);
       SyncHelper.sleep(20000);
     }
