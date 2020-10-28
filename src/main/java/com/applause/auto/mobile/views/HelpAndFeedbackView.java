933c0ebb892f9640d8564ccc4f6a5f1cfe32c9bd
Eugen Degtyarev <ujnatg@gmail.com>
2020-10-28 03:46:45 +0200

    Samsung setup

--- src/main/java/com/applause/auto/mobile/views/HelpAndFeedbackView.java
+++ src/main/java/com/applause/auto/mobile/views/HelpAndFeedbackView.java
@@ -6,6 +6,7 @@ import com.applause.auto.pageobjectmodel.annotation.Implementation;
 import com.applause.auto.pageobjectmodel.annotation.Locate;
 import com.applause.auto.pageobjectmodel.base.BaseComponent;
 import com.applause.auto.pageobjectmodel.elements.Button;
+import com.applause.auto.pageobjectmodel.elements.Text;
 import com.applause.auto.pageobjectmodel.factory.ComponentFactory;
 import com.applause.auto.util.DriverManager;
 import com.applause.auto.util.helper.SyncHelper;
@@ -14,7 +15,7 @@ import org.openqa.selenium.ScreenOrientation;
 
 import io.appium.java_client.android.AndroidDriver;
 
-@Implementation(is = HelpAndFeedbackView.class, on = Platform.MOBILE_ANDROID)
+@Implementation(is = AndroidHelpAndFeedbackView.class, on = Platform.MOBILE_ANDROID)
 @Implementation(is = HelpAndFeedbackView.class, on = Platform.MOBILE_IOS)
 public class HelpAndFeedbackView extends BaseComponent {
 
@@ -33,6 +34,19 @@ public class HelpAndFeedbackView extends BaseComponent {
       id = "com.wearehathway.peets.development:id/contactCustomer",
       on = Platform.MOBILE_ANDROID)
   protected Button contactCustomerService;
+
+  @Locate(id = "com.android.chrome:id/positive_button", on = Platform.MOBILE_ANDROID)
+  protected Text allowLocationToBrowser;
+
+  @Locate(id = "permission_allow_foreground_only_button", on = Platform.MOBILE_ANDROID)
+  protected Text allowLocationToBrowser2;
+
+  @Locate(xpath = "//XCUIElementTypeButton[@name=\"Close\"]", on = Platform.MOBILE_IOS)
+  @Locate(
+      xpath =
+          "(//android.webkit.WebView//android.widget.Button[@text='Close'])[1] | //*[@resource-id='svg-close-button'] | //span[@class='close-button']",
+      on = Platform.MOBILE_ANDROID)
+  protected Button closeAdvPopUpButton;
 
   /* -------- Actions -------- */
 
@@ -70,11 +84,6 @@ public class HelpAndFeedbackView extends BaseComponent {
     logger.info("Click View Our FAQs");
     viewOurFAQs.click();
     SyncHelper.sleep(5000);
-    AndroidDriver androidDriver = ((AndroidDriver) DriverManager.getDriver());
-    logger.info("Orientation: " + androidDriver.getOrientation());
-    logger.info("Orientation: Forcing to PORTRAIT");
-    androidDriver.rotate(ScreenOrientation.PORTRAIT);
-    logger.info("Orientation: " + androidDriver.getOrientation());
     MobileHelper.initMobileBrowser();
     // wait till the page load, before it ios is not switched back to app
     SyncHelper.sleep(10000);
@@ -92,5 +101,35 @@ public class HelpAndFeedbackView extends BaseComponent {
     SyncHelper.sleep(10000);
     MobileHelper.initMobileBrowser();
     return ComponentFactory.create(CustomerSupportScreenView.class);
+  }
+}
+
+class AndroidHelpAndFeedbackView extends HelpAndFeedbackView {
+  public PeetnikRewardsLandingView clickViewOurFAQs() {
+    logger.info("Click View Our FAQs");
+    viewOurFAQs.click();
+    SyncHelper.sleep(5000);
+    AndroidDriver androidDriver = ((AndroidDriver) DriverManager.getDriver());
+    logger.info("Orientation: " + androidDriver.getOrientation());
+    logger.info("Orientation: Forcing to PORTRAIT");
+    androidDriver.rotate(ScreenOrientation.PORTRAIT);
+    logger.info("Orientation: " + androidDriver.getOrientation());
+    try {
+      allowLocationToBrowser.click();
+    } catch (Throwable th) {
+      logger.info("No location popup overlay found");
+    }
+    try {
+      allowLocationToBrowser2.click();
+    } catch (Throwable th) {
+      logger.info("No location popup overlay found");
+    }
+    logger.info("Close popup");
+    closeAdvPopUpButton.click();
+
+    MobileHelper.initMobileBrowser();
+    // wait till the page load, before it ios is not switched back to app
+    SyncHelper.sleep(10000);
+    return ComponentFactory.create(PeetnikRewardsLandingView.class);
   }
 }
