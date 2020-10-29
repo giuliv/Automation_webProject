282ac041cb134a23b993f4b8f0bb898ef40cf683
Eugen Degtyarev <ujnatg@gmail.com>
2020-10-29 02:53:32 +0200

    Samsung setup

--- src/main/java/com/applause/auto/mobile/views/PeetnikRewardsTermsAndConditionsView.java
+++ src/main/java/com/applause/auto/mobile/views/PeetnikRewardsTermsAndConditionsView.java
@@ -8,9 +8,15 @@ import com.applause.auto.pageobjectmodel.base.BaseComponent;
 import com.applause.auto.pageobjectmodel.elements.Button;
 import com.applause.auto.pageobjectmodel.elements.Text;
 import com.applause.auto.pageobjectmodel.factory.ComponentFactory;
+import com.applause.auto.util.DriverManager;
 import com.applause.auto.util.helper.SyncHelper;
 import com.applause.auto.util.helper.sync.Until;
+
+import org.openqa.selenium.ScreenOrientation;
+
 import java.time.Duration;
+
+import io.appium.java_client.android.AndroidDriver;
 
 @Implementation(
     is = AndroidPeetnikRewardsTermsAndConditionsView.class,
@@ -32,6 +38,14 @@ public class PeetnikRewardsTermsAndConditionsView extends BaseComponent {
   @Locate(xpath = "//XCUIElementTypeButton[@name=\"Done\"]", on = Platform.MOBILE_IOS)
   protected Button doneButton;
 
+  @Locate(id = "com.android.chrome:id/positive_button", on = Platform.MOBILE_ANDROID)
+  protected Text allowLocationToBrowser;
+
+  @Locate(
+      xpath = "//android.widget.Button[@text='Allow only while using the app']",
+      on = Platform.MOBILE_ANDROID)
+  protected Text allowLocationToBrowser2;
+
   @Override
   public void afterInit() {
     SyncHelper.sleep(5000);
@@ -51,6 +65,27 @@ public class PeetnikRewardsTermsAndConditionsView extends BaseComponent {
 }
 
 class AndroidPeetnikRewardsTermsAndConditionsView extends PeetnikRewardsTermsAndConditionsView {
+
+  public void afterInit() {
+    SyncHelper.sleep(5000);
+    AndroidDriver androidDriver = ((AndroidDriver) DriverManager.getDriver());
+    logger.info("Orientation: " + androidDriver.getOrientation());
+    logger.info("Orientation: Forcing to PORTRAIT");
+    androidDriver.rotate(ScreenOrientation.PORTRAIT);
+    logger.info("Orientation: " + androidDriver.getOrientation());
+    try {
+      allowLocationToBrowser.click();
+    } catch (Throwable th) {
+      logger.info("No location popup overlay found");
+    }
+    try {
+      allowLocationToBrowser2.click();
+    } catch (Throwable th) {
+      logger.info("No location popup overlay found");
+    }
+
+    SyncHelper.wait(Until.uiElement(getHeadingText).present().setTimeout(Duration.ofSeconds(12)));
+  }
 
   /* -------- Lifecycle Methods -------- */
 
