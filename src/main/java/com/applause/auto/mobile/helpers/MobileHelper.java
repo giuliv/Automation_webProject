c4f8862c1f5c01dcdc9c429ab7e1df01e1235bb4
Eugen Degtyarev <ujnatg@gmail.com>
2020-11-07 19:23:53 +0200

    fixup

--- src/main/java/com/applause/auto/mobile/helpers/MobileHelper.java
+++ src/main/java/com/applause/auto/mobile/helpers/MobileHelper.java
@@ -383,25 +383,25 @@ public class MobileHelper {
       BaseElement element, String elementName, int maxSwipingAttempts) {
     int currentSwipingAttempts = 1;
     SyncHelper.sleep(10000);
-    int screenHeight = DeviceControl.getScreenSize().getHeight();
-    int screenWidth = DeviceControl.getScreenSize().getWidth();
 
     logger.info("Scrolling down to element: ", elementName);
     while (currentSwipingAttempts <= maxSwipingAttempts) {
       try {
-        SyncHelper.wait(Until.uiElement(element).present().setTimeout(Duration.ofSeconds(2)));
+        SyncHelper.wait(Until.uiElement(element).present().setTimeout(Duration.ofSeconds(5)));
         logger.info(elementName + " is present");
         break;
       } catch (WebDriverException e) {
         logger.info(elementName + " is not present");
         logger.info("Swipe attempt: " + currentSwipingAttempts);
+        int screenHeight = DeviceControl.getScreenSize().getHeight();
+        int screenWidth = DeviceControl.getScreenSize().getWidth();
         DeviceControl.swipeAcrossScreenCoordinates(
             screenWidth / 2,
             (int) (screenHeight * 0.75),
             screenWidth / 2,
             (int) (screenHeight * 0.4),
             1000);
-        SyncHelper.sleep(2000);
+        SyncHelper.sleep(5000);
         currentSwipingAttempts++;
         logger.info("XML Dump: ", DriverManager.getDriver().getPageSource());
       }
