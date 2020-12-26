2a5f6aa63b7365b1d2affbc979fa371709c33ed3
Eugen Degtyarev <ujnatg@gmail.com>
2020-12-26 09:23:23 +0200

    P

--- src/main/java/com/applause/auto/mobile/views/CreateAccountView.java
+++ src/main/java/com/applause/auto/mobile/views/CreateAccountView.java
@@ -14,13 +14,17 @@ import com.applause.auto.pageobjectmodel.elements.Picker;
 import com.applause.auto.pageobjectmodel.elements.Text;
 import com.applause.auto.pageobjectmodel.elements.TextBox;
 import com.applause.auto.pageobjectmodel.helper.sync.Until;
+
+import org.openqa.selenium.Dimension;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.offset.PointOption;
-import java.util.List;
-import java.util.stream.Collectors;
-import org.openqa.selenium.Dimension;
 
 @Implementation(is = AndroidCreateAccountView.class, on = Platform.MOBILE_ANDROID)
 @Implementation(is = CreateAccountView.class, on = Platform.MOBILE_IOS)
@@ -448,8 +452,8 @@ public class CreateAccountView extends BaseComponent {
     // temp case while waiter below is not working properly
 
     getSyncHelper().sleep(10000);
-    //    getSyncHelper().wait(
-    //        Until.uiElement(loadingSpinner).notPresent().setTimeout(Duration.ofSeconds(45)));
+    getSyncHelper()
+        .wait(Until.uiElement(loadingSpinner).notPresent().setTimeout(Duration.ofSeconds(45)));
     return this.create(DashboardView.class);
   }
 
@@ -614,8 +618,7 @@ public class CreateAccountView extends BaseComponent {
     logger.info("Checking password text displayed");
     getHiddenPasswordTextBox.sendKeys(" ");
     boolean result =
-        getPasswordHintTextBox
-            .stream()
+        getPasswordHintTextBox.stream()
             .map(item -> item.getText())
             .collect(Collectors.joining("\n"))
             .equals("At least 6 characters\n" + "At least 1 number\n" + "At least 1 letter");
@@ -793,8 +796,7 @@ class AndroidCreateAccountView extends CreateAccountView {
     logger.info("Checking password text displayed");
     getHiddenPasswordTextBox.sendKeys(" ");
     boolean result =
-        getPasswordHintTextBox
-            .stream()
+        getPasswordHintTextBox.stream()
             .map(item -> item.getText())
             .collect(Collectors.joining("\n"))
             .equals("At least 6 characters\n" + "At least 1 number\n" + "At least 1 letter");
