bc45f303b6fe18e18861cb635ae61b44bfbd1ef4
tonyMedina125 <tony.medina125@gmail.com>
2021-06-20 20:21:36 -0500

    tx05

--- src/main/java/com/applause/auto/new_web/components/Header.java
+++ src/main/java/com/applause/auto/new_web/components/Header.java
@@ -11,6 +11,7 @@ import com.applause.auto.pageobjectmodel.base.BaseComponent;
 import com.applause.auto.pageobjectmodel.elements.Button;
 import com.applause.auto.pageobjectmodel.elements.ContainerElement;
 import com.applause.auto.helpers.sync.Until;
+import com.applause.auto.new_web.views.SignInPage;
 
 @Implementation(is = Header.class, on = Platform.WEB)
 @Implementation(is = HeaderMobile.class, on = Platform.WEB_MOBILE_PHONE)
@@ -36,6 +37,10 @@ public class Header extends BaseComponent {
 
   @Locate(css = ".header__mobile-menu", on = Platform.WEB_MOBILE_PHONE)
   protected Button hamburgerButton;
+
+  @Locate(css = "#header a[href*='/login'] i", on = Platform.WEB)
+  @Locate(css = "#navMobileMain a[href*='/login']", on = Platform.WEB_MOBILE_PHONE)
+  protected Button signInButton;
 
   @Override
   public void afterInit() {
@@ -75,6 +80,13 @@ public class Header extends BaseComponent {
 
     return SdkHelper.create(clazz);
   }
+
+  public SignInPage clickSignInButton() {
+    logger.info("Tap on SignIn Button");
+    SdkHelper.getSyncHelper().wait(Until.uiElement(signInButton).clickable()).click();
+
+    return SdkHelper.create(SignInPage.class);
+  }
 }
 
 class HeaderMobile extends Header {
@@ -110,5 +122,15 @@ class HeaderMobile extends Header {
     hamburgerButton.click();
 
     SdkHelper.getSyncHelper().wait(Until.uiElement(coffeeCategory).visible());
+  }
+
+  @Override
+  public SignInPage clickSignInButton() {
+    openHamburgerMenu();
+
+    logger.info("Tap on SignIn Button");
+    SdkHelper.getSyncHelper().wait(Until.uiElement(signInButton).clickable()).click();
+
+    return SdkHelper.create(SignInPage.class);
   }
 }
